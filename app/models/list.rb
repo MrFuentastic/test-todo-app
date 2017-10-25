82e025bae24d90aacfc4c349b2337bda09f70690
MrFuentastic <vfuentes8587@gmail.com>
2017-10-24 21:30:17 -0500

    added rspec tests

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,9 +2,7 @@ class List < ApplicationRecord
   has_many :tasks
 
   def complete_all_tasks!
-    tasks.each do |task|
-      task.update(complete: true)
-    end
+      tasks.update_all(complete: true)
   end
 
   def snooze_all_tasks!
@@ -14,30 +12,14 @@ class List < ApplicationRecord
   end
 
   def total_duration
-    total = 0
-    tasks.each do |task|
-      total += task.duration
-    end
-    return total
+    tasks.reduce(0) {|sum, task| sum + task.duration}
   end
 
   def incomplete_tasks
-    array_of_tasks = []
-    tasks.each do |task|
-      if !task.complete
-        array_of_tasks << task
-      end
-    end
-    return array_of_tasks
+    tasks.select {|task| !task.complete}
   end
 
   def favorite_tasks
-    array_of_tasks = []
-    tasks.each do |task|
-      if task.favorite
-        array_of_tasks << task
-      end
-    end
-    return array_of_tasks
+    tasks.select {|task| task.favorite}
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,19 +5,78 @@ RSpec.describe List, type: :model do
   describe '#complete_all_tasks!' do
     it 'should change the complete attribute off all tasks in list to true' do
       chores = List.create(name:"chores")
-      Task.create(list_id: chores.id, name:"garbage", complete:true, favorite:false, deadline:1.month.from_now)
-      Task.create(list_id: chores.id, name:"mow lawn", complete:false, favorite:false, deadline:5.months.ago)
-      Task.create(list_id: chores.id, name:"dishes", complete:true, favorite:true, deadline:1.day.ago)
-      Task.create(list_id: chores.id, name:"walk z dog", complete:true, favorite:true, deadline:1.hour.ago)
+      Task.create(list_id: chores.id, name:"garbage", complete:true)
+      Task.create(list_id: chores.id, name:"mow lawn", complete:false)
+      Task.create(list_id: chores.id, name:"dishes", complete:true)
+      Task.create(list_id: chores.id, name:"walk z dog", complete:true)
+
       chores.complete_all_tasks!
-      method_worked = true
+
       chores.tasks.each do |task|
-        if task.complete == false
-          method_worked = false
-        end
+        expect(task.complete).to eq(true)
       end
-      expect(method_worked).to eq(true)
     end
+  end
+
+  describe '#snooze_all_tasks!' do
+    it 'should complete the snooze hour method for each task' do
+      chores = List.create(name:"chores")
+      Task.create(list_id: chores.id, name:"garbage", deadline:1.month.from_now)
+      Task.create(list_id: chores.id, name:"mow lawn",deadline:5.months.ago)
+      Task.create(list_id: chores.id, name:"dishes", deadline:1.day.ago)
+      Task.create(list_id: chores.id, name:"walk z dog", deadline:1.hour.ago)
+      
+      tasks_0_deadline = chores.tasks[0].deadline
+      tasks_1_deadline = chores.tasks[1].deadline
+      tasks_2_deadline = chores.tasks[2].deadline
+      tasks_3_deadline = chores.tasks[3].deadline
+      
+      chores.snooze_all_tasks!
+
+      expect(chores.tasks[0].deadline).to eq(tasks_0_deadline + 1.hour)
+      expect(chores.tasks[1].deadline).to eq(tasks_1_deadline + 1.hour)
+      expect(chores.tasks[2].deadline).to eq(tasks_2_deadline + 1.hour)
+      expect(chores.tasks[3].deadline).to eq(tasks_3_deadline + 1.hour)
+    end
+  end
+
+  describe '#total_duration' do
+    it 'should return the sum of the durations for all tasks' do
+      chores = List.create(name:"chores")
+      Task.create(list_id: chores.id, name:"garbage", duration: 100)
+      Task.create(list_id: chores.id, name:"mow lawn", duration: 100)
+      Task.create(list_id: chores.id, name:"dishes", duration: 100)
+      Task.create(list_id: chores.id, name:"walk z dog", duration: 100)
+
+      expect(chores.total_duration).to eq(400)
+    end
+  end
+
+  describe '#incomplete_tasks' do
+    it 'should return an array of the tasks that have a false complete attribute' do
+      chores = List.create(name:"chores")
+      Task.create(list_id: chores.id, name:"garbage", complete:true)
+      Task.create(list_id: chores.id, name:"mow lawn", complete:false)
+      Task.create(list_id: chores.id, name:"dishes", complete:false)
+      Task.create(list_id: chores.id, name:"walk z dog", complete:true)
+      
+      test_array = [chores.tasks[1], chores.tasks[2]]
+      expect(chores.incomplete_tasks).to eq(test_array)
+    end
+  end
+
+  describe '#favorite_tasks' do
+    it 'should return an array of tasks with a true favorite attribute' do
+      chores = List.create(name:"chores")
+      Task.create(list_id: chores.id, name:"garbage", favorite:false)
+      Task.create(list_id: chores.id, name:"mow lawn", favorite:false)
+      Task.create(list_id: chores.id, name:"dishes", favorite:true)
+      Task.create(list_id: chores.id, name:"walk z dog", favorite:true)
+      
+      test_array = [chores.tasks[2], chores.tasks[3]]
+      expect(chores.favorite_tasks).to eq(test_array)
+    end
+
   end
 
 end
